69bf611ea69d3c2f378654cefd62b9ec33876550
Lincoln Karuhanga <karuhanga475@gmail.com>
2019-08-05 02:11:36 +0300

    Setup test

--- src/app.py
+++ src/app.py
@@ -3,17 +3,24 @@ from spellchecker import SpellChecker
 
 luganda_checker = None
 runyankole_checker = None
-luganda_checker = SpellChecker(language=None, case_sensitive=False)
-luganda_checker.word_frequency.load_dictionary('luganda.json')
-
-runyankole_checker = SpellChecker(language=None, case_sensitive=False)
-runyankole_checker.word_frequency.load_dictionary('runya.json')
 
 app = Flask(__name__)
 
 
+def setup():
+    global luganda_checker, runyankole_checker
+    if not luganda_checker:
+        luganda_checker = SpellChecker(language=None, case_sensitive=False)
+        luganda_checker.word_frequency.load_dictionary('luganda.json')
+
+    if not runyankole_checker:
+        runyankole_checker = SpellChecker(language=None, case_sensitive=False)
+        runyankole_checker.word_frequency.load_dictionary('runya.json')
+
+
 @app.route("/suggest/luganda/<string:word>")
 def suggestions_luganda(word):
+    setup()
     candidates = luganda_checker.candidates(word)
     candidates = list(candidates)
     return jsonify(candidates)
@@ -21,6 +28,7 @@ def suggestions_luganda(word):
 
 @app.route("/suggest/runyankole/<string:word>")
 def suggestions_runyankole(word):
+    setup()
     candidates = runyankole_checker.candidates(word)
     candidates = list(candidates)
     return jsonify(candidates)
@@ -28,26 +36,31 @@ def suggestions_runyankole(word):
 
 @app.route("/is-known/luganda/<string:word>")
 def is_known_luganda(word):
+    setup()
     return jsonify(len(luganda_checker.known([word.lower()])) > 0)
 
 
 @app.route("/is-known/runyankole/<string:word>")
 def is_known_runyankole(word):
+    setup()
     return jsonify({'isKnown': len(runyankole_checker.known([word.lower()])) > 0})
 
 
 @app.route('/')
 def home():
+    setup()
     return render_template("speller.html")
 
 
 @app.route('/luganda')
 def luganda():
+    setup()
     return render_template("luganda.html")
 
 
 @app.route('/runyankole')
 def runyankole():
+    setup()
     return render_template("runyankole.html")
 
 
